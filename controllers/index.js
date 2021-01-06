48d53ca6d0cd9dcf6e6a3f08129326d45b4e23b3
Adam Barron <adambarron@me.com>
2021-01-05 22:27:37 -0800

    added dashboard page and routes

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
+const dashbaordRoutes = require('./dashboard-routes');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/dashboard', dashbaordRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
